7738761a4f4e09827b56395992b160c4182882b8
Piyush <01fe22bca214@kletech.ac.in>
2024-03-31 23:27:31 +0530

    Merging

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -6,21 +6,3 @@ public class FibonacciSeries {
         obj.fibonacci(N);
     }
 }
-class Main {
-    
-    static void fibonacci(int N)
-    {
-        int num1 = 0, num2 = 1;
-      
-        for (int i = 0; i < N; i++) {
-            
-            System.out.print(num1 + " ");
-
-           
-            int num3 = num2 + num1;
-            num1 = num2;
-            num2 = num3;
-        }
-    }
-    
-}
